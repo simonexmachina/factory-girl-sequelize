567d28fcdaf9215a446ddea7346cd12352090ee0
Simon Wade <simon.wade@gmail.com>
2014-12-08 10:01:33 +1100

    Change to use in-memory SQLite

--- config/test.js
+++ config/test.js
@@ -2,7 +2,7 @@ module.exports = {
   database: 'factory-girl-sequelize',
   options: {
     dialect: 'sqlite',
-    storage: 'test/tmp/test.db',
+    storage: ':memory:',
     logging: null
   }
 };
